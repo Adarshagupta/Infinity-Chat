3a694ebd49d2bcd993edbe01e383e99b3e931b55
Adarsh Gupta <blueadarsh1@gmail.com>
2024-10-12 13:04:48 +0545

    call back url fix

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if "user_id" not in session:
-            return redirect(url_for("login"))
+            return redirect(url_for("auth"))
         return f(*args, **kwargs)
     return decorated_function
 
